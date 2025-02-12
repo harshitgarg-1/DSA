1eec80bc7f7effac87c7f049ac2f854719439460
harshitgarg-1 <112110383+harshitgarg-1@users.noreply.github.com>
2025-02-12 18:11:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (87.08%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -4,8 +4,6 @@ class Solution {
         dp[1] = 1;
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
-                if(i==1 && j==1){
-                }
                 dp[j] = dp[j] + dp[j-1];
             }
             
