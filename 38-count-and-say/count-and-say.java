751680d309cb0de8550d4ce44bad8126e997e52f
harshitgarg-1 <112110383+harshitgarg-1@users.noreply.github.com>
2025-02-16 17:49:39 +0530

    Time: 1 ms (99.57%) | Memory: 41.4 MB (68.67%) - LeetSync

---
+++ 38-count-and-say/count-and-say.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String countAndSay(int n) {
+     if(n==1)return "1";
+     String p=countAndSay(n-1);
+     return encode(p);
+    }
+    public String encode(String p){
+        int q=p.length();
+        StringBuilder sb=new StringBuilder();
+        int count=1;
+        for(int i=1;i<q;i++){
+            if(p.charAt(i) == p.charAt(i-1)){
+                count++;
+            }
+            else{
+                sb.append(count).append(p.charAt(i-1));
+                count=1;
+            }
+        }   
+        sb.append(count).append(p.charAt(q-1));
+        return sb.toString();
+    }
+}
