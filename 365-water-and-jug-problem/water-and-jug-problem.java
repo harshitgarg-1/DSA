fc02b51c2edad26a3dadb8f487cc036a2cfbc3db
harshitgarg-1 <112110383+harshitgarg-1@users.noreply.github.com>
2025-03-06 20:48:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (49.05%) - LeetSync

---
+++ 365-water-and-jug-problem/water-and-jug-problem.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canMeasureWater(int x, int y, int target) {
+        int u,max=0;
+        if(x>y){u=x;}else{u=y;}
+        if(x+y <target ){return false;}
+
+        //gcd
+       for(int i=1;i<=u;i++)
+       {
+           if(x%i==0&&y%i==0)
+           {
+               max=Math.max(i,max);
+           }
+       }
+       if(target%max==0){return true;}
+
+
+        
+        return false;
+    }
+}
