aaa5645807a5945ba987f63a543b1b4b4701e5d6
harshitgarg-1 <112110383+harshitgarg-1@users.noreply.github.com>
2025-03-08 12:30:22 +0530

    Time: 147 ms (14.49%) | Memory: 55 MB (6.23%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,18 +1,28 @@
 class Solution {
     public int numSquares(int n) {
-
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return findMin(n, dp);
+        List<Integer> ps = new ArrayList<>();
+        for(int i=1;i*i<=n;i++){
+            ps.add(i*i);
+        }
+        int m = ps.size();
+        int[][] dp = new int[m][n+1];
+        for(int i=0;i<m;i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return findMin(m-1, n, dp, ps);
     }
 
-    public int findMin(int target, int[] dp){
-        if(target==0) return 0;
-        if(dp[target]!=-1) return dp[target];
-        int ans = Integer.MAX_VALUE;
-        for(int i=1;i*i<=target;i++){
-            ans = Math.min(ans, 1+findMin(target-i*i, dp));
+    public int findMin(int i, int target, int[][] dp, List<Integer> ps){
+        if(i==0){
+            return target;
         }
-        return dp[target]=ans;
+        if(target==0) return 0;
+        if(dp[i][target]!=-1) return dp[i][target];
+        int ans = Integer.MAX_VALUE;
+        ans = Math.min(ans, findMin(i-1, target, dp, ps));
+        if(ps.get(i)<=target){
+            ans = Math.min(ans, 1+findMin(i, target-ps.get(i), dp, ps));
+        }
+        return dp[i][target] = ans;
     }
 }
