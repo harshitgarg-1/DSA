01ef2ef8672bac0ced43af863642aaffc79d2f98
harshitgarg-1 <112110383+harshitgarg-1@users.noreply.github.com>
2025-03-27 09:45:00 +0530

    Time: 319 ms (9.09%) | Memory: 93.3 MB (30.58%) - LeetSync

---
+++ 1733-maximum-number-of-visible-points/maximum-number-of-visible-points.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int visiblePoints(List<List<Integer>> points, int angle, List<Integer> location) {
+        List<Double> degree = new ArrayList<>();
+        int x = location.get(0);
+        int y = location.get(1);
+        int origin = 0;
+        for(List<Integer> point : points){
+            if(point.get(0)==x && point.get(1)==y) {
+                origin++;
+                continue;
+            }
+            degree.add(Math.toDegrees(Math.atan2(point.get(1)-y, point.get(0)-x)));
+        }
+        Collections.sort(degree);
+        int n = degree.size();
+        for(int i=0;i<n;i++){
+            degree.add(360+degree.get(i));
+        }
+        System.out.println(degree);
+        int ans = 1;
+        if(degree.size()==0) ans=0;
+        int i=0,j=1;
+        while(j<degree.size()){
+            while(degree.get(j)-degree.get(i)>angle) i++;
+            ans = Math.max(ans, j-i+1);
+            j++;
+        }
+        return ans+origin;
+
+    }
+}
