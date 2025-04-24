ec05d5535a500e7e34baf9ea63d25fd4fa94aed2
harshitgarg-1 <112110383+harshitgarg-1@users.noreply.github.com>
2025-04-24 09:25:52 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (99.26%) - LeetSync

--- 343-integer-break/integer-break.java
+++ 343-integer-break/integer-break.java
@@ -1,6 +1,5 @@
 class Solution {
     public int integerBreak(int n) {
-        //factors should be less than 4, otherwise we can do the break and get a better product. The factors in last result should be 1, 2 or 3. Obviously, 1 should be abandoned. Thus, the factors of the perfect product should be 2 or 3.
         if(n==2) return 1;
         if(n==3) return 2;
         int product = 1;
