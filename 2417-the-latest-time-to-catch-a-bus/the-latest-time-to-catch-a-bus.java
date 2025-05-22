3136af4276960be238abb7a141f3753d18b4a1c7
harshitgarg-1 <112110383+harshitgarg-1@users.noreply.github.com>
2025-05-22 07:17:35 +0530

    Time: 27 ms (76.19%) | Memory: 61.9 MB (56.83%) - LeetSync

--- 2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.java
+++ 2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.java
@@ -2,18 +2,18 @@ class Solution {
     public int latestTimeCatchTheBus(int[] buses, int[] passengers, int capacity) {
         Arrays.sort(buses);
         Arrays.sort(passengers);
-        Set<Integer> set = new HashSet<>();
+        int last = -1;
         int ans=0,j=0;
         for(int i=0;i<buses.length;i++){
             int c = 0;
             while(j<passengers.length && c<capacity && passengers[j]<=buses[i]){
-                if(!set.contains(passengers[j]-1)){
+                if(last != passengers[j]-1){
                     ans=passengers[j]-1;
                 }
-                set.add(passengers[j]);
+                last = passengers[j];
                 j++; c++;
             }
-            if(c<capacity && !set.contains(buses[i])){
+            if(c<capacity && last != buses[i]){
                 ans = buses[i];
             }
         }
