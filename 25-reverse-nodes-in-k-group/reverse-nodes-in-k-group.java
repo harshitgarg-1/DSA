def7488a9b04531b6db0cecd53ec7664768c8e1d
harshitgarg-1 <112110383+harshitgarg-1@users.noreply.github.com>
2025-09-03 14:12:02 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (32.88%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -10,48 +10,47 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode prev = null, newHead = null, curr = head;
-        while(curr != null){
-            ListNode next = null;
-            ListNode kth = kthNode(curr, k);
-            if(kth==null){
-                if(prev==null){
-                    newHead = curr;
-                } else {
+        ListNode curr = head, root = head, next = null, prev = null;
+        while(curr != null) {
+            ListNode end = findKthNode(curr, k);
+            if(end == null){
+                if(curr != root){
                     prev.next = curr;
                 }
                 curr = null;
             } else {
-                next = kth.next;
-                kth.next = null;
+                next = end.next;
+                end.next = null;
                 reverse(curr);
-                if(prev==null){
-                    newHead = kth;
-                } else {
-                    prev.next = kth;
+                if(curr == root){
+                    root = end;
+                }
+                if(prev != null){
+                    prev.next = end;
                 }
                 prev = curr;
                 curr = next;
             }
         }
-        return newHead;
+        return root;
     }
-    public ListNode kthNode(ListNode head, int k) {
-        while(head != null && k>1) {
-            head = head.next;
+    public ListNode findKthNode(ListNode curr, int k){
+        ListNode ans = curr;
+        while(ans != null && k>1){
+            ans = ans.next;
             k--;
         }
-        return head;
-    }
-    public ListNode reverse(ListNode head) {
-        ListNode prev = null, curr = head;
-        while(curr != null){
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
+        return ans;
     }
 
+    public void reverse(ListNode curr){
+        ListNode head = curr, prev = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+
+        }
+    }
 }
