ec5bb0eb377eaf2ec31858690e6ce13563b84d86
harshitgarg-1 <112110383+harshitgarg-1@users.noreply.github.com>
2025-09-05 07:19:57 +0530

    Time: 6 ms (83.23%) | Memory: 44.3 MB (70.60%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,27 +1,35 @@
+import java.util.*;
+
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         int[] price = new int[n];
-        Arrays.fill(price, 10000000);
+        Arrays.fill(price, Integer.MAX_VALUE);
+        price[src] = 0;
+
         ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
-        for(int[] flight : flights){
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        for (int[] flight : flights) {
             adj.get(flight[0]).add(new int[]{flight[1], flight[2]});
         }
+
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{0, 0, src});
-        price[src] = 0;
-        while(!q.isEmpty()){
+
+        while (!q.isEmpty()) {
             int[] curr = q.poll();
-            if(curr[1]>k) continue;
-            for(int[] neb : adj.get(curr[2])){
-                int des = neb[0];
-                int p = neb[1];
-                if(price[des]>p+curr[0]){
-                    price[des] = p+curr[0];
-                    q.add(new int[]{price[des], curr[1]+1, des});
+            int currCost = curr[0];
+            int currStops = curr[1];
+            int currNode = curr[2];
+            if (currStops > k) continue;
+            for (int[] neighbor : adj.get(currNode)) {
+                int nextNode = neighbor[0];
+                int flightCost = neighbor[1];
+                if (currCost + flightCost < price[nextNode]) {
+                    price[nextNode] = currCost + flightCost;
+                    q.add(new int[]{price[nextNode], currStops + 1, nextNode});
                 }
             }
         }
-        return price[dst] == 10000000 ? -1 : price[dst];
+        return price[dst] == Integer.MAX_VALUE ? -1 : price[dst];
     }
 }
