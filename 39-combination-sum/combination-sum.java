446ee86a451576777988e1f0a2b2ec9bac3ca2ce
harshitgarg-1 <112110383+harshitgarg-1@users.noreply.github.com>
2025-10-19 08:24:00 +0530

    Time: 5 ms (6.78%) | Memory: 44.9 MB (20.23%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,21 +1,21 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        generate(candidates, 0, target, new ArrayList<>(), ans);
-        return ans;
-    }
+        Arrays.sort(candidates);
+        List<List<List<Integer>>> dp = new ArrayList<>(target + 1);
+        for (int i = 0; i <= target; i++) dp.add(new ArrayList<>());
 
-    public void generate(int[] candidates, int i, int target, List<Integer> curr, List<List<Integer>> ans){
-        if(target==0){
-            ans.add(new ArrayList<>(curr));
-            return;
+        dp.get(0).add(new ArrayList<>());
+
+        for (int c : candidates) {
+            for (int t = c; t <= target; t++) {
+                for (List<Integer> prev : dp.get(t - c)) {
+                    List<Integer> newList = new ArrayList<>(prev);
+                    newList.add(c);
+                    dp.get(t).add(newList);
+                }
+            }
         }
-        if(i==candidates.length) return;
-        generate(candidates, i+1, target, curr, ans);
-        if(candidates[i]<=target){
-            curr.add(candidates[i]);
-            generate(candidates, i, target-candidates[i], curr, ans);
-            curr.remove(curr.size()-1);
-        }
+
+        return dp.get(target);
     }
 }
