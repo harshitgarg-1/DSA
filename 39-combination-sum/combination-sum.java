a946257b1b771f4404216fda03a38d83cef8f234
harshitgarg-1 <112110383+harshitgarg-1@users.noreply.github.com>
2025-10-19 08:24:19 +0530

    Time: 10 ms (5.06%) | Memory: 44.9 MB (20.23%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,21 +1,34 @@
+import java.util.*;
+
 class Solution {
+    Map<String, List<List<Integer>>> memo = new HashMap<>();
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
-        List<List<List<Integer>>> dp = new ArrayList<>(target + 1);
-        for (int i = 0; i <= target; i++) dp.add(new ArrayList<>());
+        return dfs(candidates, target, 0);
+    }
 
-        dp.get(0).add(new ArrayList<>());
+    private List<List<Integer>> dfs(int[] candidates, int target, int start) {
+        String key = start + "," + target;
+        if (memo.containsKey(key)) return memo.get(key);
+        
+        List<List<Integer>> res = new ArrayList<>();
+        if (target == 0) {
+            res.add(new ArrayList<>());
+            return res;
+        }
 
-        for (int c : candidates) {
-            for (int t = c; t <= target; t++) {
-                for (List<Integer> prev : dp.get(t - c)) {
-                    List<Integer> newList = new ArrayList<>(prev);
-                    newList.add(c);
-                    dp.get(t).add(newList);
-                }
+        for (int i = start; i < candidates.length; i++) {
+            if (candidates[i] > target) break;
+            for (List<Integer> sub : dfs(candidates, target - candidates[i], i)) {
+                List<Integer> newList = new ArrayList<>(sub);
+                newList.add(0, candidates[i]);
+                res.add(newList);
             }
         }
 
-        return dp.get(target);
+        memo.put(key, res);
+        return res;
     }
 }
+
