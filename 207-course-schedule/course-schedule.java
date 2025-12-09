40e9ed1d6ef5414379bdc3e39c6ac625b9f61c62
harshitgarg-1 <112110383+harshitgarg-1@users.noreply.github.com>
2025-12-09 17:41:47 +0530

    Time: 4 ms (92.04%) | Memory: 47.3 MB (19.47%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -8,26 +8,24 @@ class Solution {
             adjList.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
         int[] vis = new int[numCourses];
-        Stack<Integer> st = new Stack<>();
         for(int i=0;i<numCourses;i++){
             if(vis[i]==0){
-                if(checkCycle(i, adjList, vis, st)){
+                if(checkCycle(i, adjList, vis)){
                     return false;
                 }
             }
         }
         return true;
     }
-    public boolean checkCycle(int node, ArrayList<ArrayList<Integer>> adjList, int[] vis, Stack<Integer> st){
+    public boolean checkCycle(int node, ArrayList<ArrayList<Integer>> adjList, int[] vis){
         vis[node] = 2;
         for(int neb : adjList.get(node)){
             if(vis[neb]==2) return true;
-            if(vis[neb]==0 && checkCycle(neb, adjList, vis, st)){
+            if(vis[neb]==0 && checkCycle(neb, adjList, vis)){
                 return true;
             }
         }
         vis[node] = 1;
-        st.push(node);
         return false;
     }
 }
