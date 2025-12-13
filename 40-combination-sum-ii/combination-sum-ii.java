5fb43166e75d39efb1831af43f420b323f3c4b45
harshitgarg-1 <112110383+harshitgarg-1@users.noreply.github.com>
2025-12-13 11:24:14 +0530

    Time: 11 ms (11.62%) | Memory: 47 MB (7.95%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,23 +1,34 @@
+import java.util.*;
+
 class Solution {
+    Map<String, List<List<Integer>>> memo = new HashMap<>();
+
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(candidates);
-        generate(candidates, 0, target, new ArrayList<>(), ans);
-        return ans;
+        return dfs(candidates, target, 0);
     }
 
-    public void generate(int[] candidates, int i, int target, List<Integer> curr, List<List<Integer>> ans){
-        if(target==0){
-            ans.add(new ArrayList<>(curr));
-            return;
+    private List<List<Integer>> dfs(int[] candidates, int target, int start) {
+        String key = start + "," + target;
+        if (memo.containsKey(key)) return memo.get(key);
+        
+        List<List<Integer>> res = new ArrayList<>();
+        if (target == 0) {
+            res.add(new ArrayList<>());
+            return res;
         }
-        if(i==candidates.length) return;
-        if(candidates[i]<=target){
-            curr.add(candidates[i]);
-            generate(candidates, i+1, target-candidates[i], curr, ans);
-            curr.remove(curr.size()-1);
+
+        for (int i = start; i < candidates.length; i++) {
+            if (candidates[i] > target) break;
+            if(i>start && candidates[i]==candidates[i-1]) continue;
+            for (List<Integer> sub : dfs(candidates, target - candidates[i], i+1)) {
+                List<Integer> newList = new ArrayList<>(sub);
+                newList.add(0, candidates[i]);
+                res.add(newList);
+            }
         }
-        while(i<candidates.length-1 && candidates[i]==candidates[i+1]) i++;
-        generate(candidates, i+1, target, curr, ans);
+
+        memo.put(key, res);
+        return res;
     }
 }
